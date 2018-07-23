83d07851aff9611102c2cee75f0dfde2e77247db
xiaojinzi <347837667@qq.com>
2018-07-23 15:55:59 +0800

    Initial commit

---
+++ componentlib/src/main/java/com/luojilab/component/componentlib/applicationlike/IApplicationLike.java
@@ -0,0 +1,13 @@
+package com.luojilab.component.componentlib.applicationlike;
+
+/**
+ * Created by mrzhang on 2017/6/15.
+ */
+
+public interface IApplicationLike {
+
+    void onCreate();
+
+    void onStop();
+
+}
